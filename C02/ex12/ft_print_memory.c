a9a1f6b64fc9fdaa5a13b069a9b274aaaff706f3
Kelyan Csajka <kcsajka@c1r14s2.42lausanne.ch>
2024-08-27 16:39:56 +0200

    update c02/ex12

--- C02/ex12/ft_print_memory.c
+++ C02/ex12/ft_print_memory.c
@@ -6,7 +6,7 @@
 /*   By: kcsajka <kcsajka@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 15:12:41 by kcsajka           #+#    #+#             */
-/*   Updated: 2024/08/26 16:29:12 by kcsajka          ###   ########.fr       */
+/*   Updated: 2024/08/27 16:34:35 by kcsajka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,16 +23,10 @@ void	ft_putstr(char *str)
 		ft_putchar(*str++);
 }
 
-int ft_pow(int base, int exp)
+int ft_is_printable(char c)
 {
-	int res;
-
-	res = 1;
-	while (exp-- > 0)
-		res *= base;
-	return (res);
+	return (32 <= c && c <= 126);
 }
-
 void	ft_puthex_addr_helper(int i, int depth, int pad, char const *table)
 {
 	while (i == 0 && depth++ < pad)
@@ -45,30 +39,70 @@ void	ft_puthex_addr_helper(int i, int depth, int pad, char const *table)
 	ft_putchar(table[i % 16]);
 }
 
-void	ft_put_hex_addr(void *addr)
+void	ft_put_hex(int v, int pad)
 {
 	char const *table;
 
 	table = "0123456789abcdef";
-	ft_puthex_addr_helper((int)addr, 0, 16, table);
+	ft_puthex_addr_helper(v, 0, pad, table);
 }
+
+int ft_get_int_from_ptr(char *ptr)
+{
+	return *(char *)ptr;
+}
+
+void ft_print_addr_value(void *addr, unsigned int size)
+{
+	unsigned int	i;
+	char value;
+
+	i = 0;
+	while (i < size)
+	{
+		value = ft_get_int_from_ptr(addr + i);
+		if (ft_is_printable(value))
+			ft_putchar(value);
+		else
+			ft_putchar('.');
+		i++;
+	}
+}
+
 
 void	*ft_print_memory(void *addr, unsigned int size)
 {
 	unsigned int	i;
-	int row;
-
+	int col;
+	
 	i = 0;
-	row = 0;
-	while (i < size)
+	col = 16;
+	while (i < size + size % 16)
 	{
-		if (row == 0)
+		if (col == 16)
 		{
-			ft_put_hex_addr(addr);
+			if (i > 1)
+			{
+				ft_print_addr_value(addr, 16);
+				ft_putchar('\n');
+				addr+= 16;
+			}
+			ft_put_hex((int)addr, 16);
+			ft_putstr(": ");
+			col = 0;
+			continue ;
 		}
-
-		addr++;
-		i++;
+		if (col < 16)
+		{
+			if (i < size)
+				ft_put_hex(ft_get_int_from_ptr(addr + col), 2);
+			else
+				ft_putstr("  ");
+			if (i % 2 == 1)
+				ft_putchar(' ');
+			col += 1;
+			i += 1;
+		}
 	}
 
 	return (addr);
--- C02/ex12/main.c
+++ C02/ex12/main.c
@@ -1,12 +1,23 @@
+#include <string.h>
 #include <stdio.h>
 
 void	*ft_print_memory(void *addr, unsigned int size);
 void	*ft_put_hex_addr(void *addr);
+int ft_get_int_from_ptr(void *ptr);
 int	ft_pow(int base, int exp);
+
+char ft_get_long_from_ptr(void *ptr)
+{
+	return *(char *)ptr;
+}
 
 int main()
 {
-	//printf("%d\n", ft_pow(16, 4));
-	char *str1 = "lol";
-	ft_print_memory(str1, 4);
+	char *c = "bonjour les aminches c'est fou tout ce qu'on peut faire avec printmemory";
+	int a = 1642;
+	char *c2 = "\tlol\nlol c'est\ndingue";
+	(void)a;
+	(void)c2;
+	//printf("%u\n", ft_get_long_from_ptr(&c));
+	ft_print_memory(c, 92);
 }
