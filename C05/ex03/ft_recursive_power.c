27b171f3e3f6f406ff2e69a227bb0e2625c40899
ckelyan <66877262+ckelyan@users.noreply.github.com>
2024-09-06 14:50:21 +0200

    fix c05/ex03

--- C05/ex03/ft_recursive_power.c
+++ C05/ex03/ft_recursive_power.c
@@ -6,15 +6,15 @@
 /*   By: kcsajka <kcsajka@student.42lausanne.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/01 19:03:29 by kcsajka           #+#    #+#             */
-/*   Updated: 2024/09/02 16:02:37 by kcsajka          ###   ########.fr       */
+/*   Updated: 2024/09/06 11:14:46 by kcsajka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_recursive_power(int n, int pow)
 {
-	if (pow < 0 || n == 0)
+	if (pow < 0)
 		return (0);
-	if (pow < 2 || n == 1)
+	if (pow == 0)
 		return (1);
 	return (n * ft_recursive_power(n, pow - 1));
 }
