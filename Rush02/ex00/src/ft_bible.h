300f0b913d522e02462ff5fe9bf6db4afe01d232
Kely Csajka <kcsajka@student.42lausanne.ch>
2024-09-10 19:09:53 +0200

    Update rush02 from project repo

--- Rush02/ex00/src/ft_bible.h
+++ Rush02/ex00/src/ft_bible.h
@@ -6,7 +6,7 @@
 /*   By: jruiz <marvin@42lausanne.ch>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/07 01:09:29 by jruiz             #+#    #+#             */
-/*   Updated: 2024/09/07 11:29:51 by jruiz            ###   ########.fr       */
+/*   Updated: 2024/09/08 20:22:09 by kcsajka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,39 @@
 # include <unistd.h>
 # include <string.h>
 # include <fcntl.h>
-# include <stdlib>
+# include <stdlib.h>
+# include <stdio.h>
 
-int		ft_atoi(char *str);
-int		ft_strlens(char *str);
-char	ft_verif(char *str);
+// parsing
+typedef struct s_value
+{
+	char	*lvalue;
+	char	*rvalue;
+	int		power;
+}	t_value;
+t_value	*ft_readdict(t_value *res, char *filename);
+int		ft_count_lines(char *filename);
+void	ft_free_dict(t_value *dict);
+
+// memory
+char	**malloc_all(char *str);
+void	fill_tab(char **tab, char *str);
+void	free_all_tab(char **tab);
+void	free_all_tab_i(char **tab, int i);
+
+// utils
+int		ft_strlen(char *str);
+void	ft_putstr(char *str);
+int		ft_is_space(char c);
+char	*ft_strdup(char *str, int size);
+int		ft_verify(char *str);
+
+// solver
+void	write_all(t_value *dict, char *str);
+void	write_number(t_value *dict, char *tab, int power);
+t_value	*find_by_power(t_value *values, int pow);
+t_value	*find_by_lvalue(t_value *values, char s[4]);
+void	print_first_number(t_value *dict, char *tab);
+void	print_second_number(t_value *dict, char *tab);
+void	print_third_number(t_value *dict, char *tab);
 #endif
--- Rush02/ex00/src/main.c
+++ Rush02/ex00/src/main.c
@@ -3,29 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jruiz <marvin@42lausanne.ch>               +#+  +:+       +#+        */
+/*   By: kcsajka <kcsajka@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/06 22:47:27 by jruiz             #+#    #+#             */
-/*   Updated: 2024/09/07 11:18:44 by jruiz            ###   ########.fr       */
+/*   Created: 2024/09/08 16:59:56 by kcsajka           #+#    #+#             */
+/*   Updated: 2024/09/08 20:28:43 by kcsajka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_bible.h"
 
-int	main(int argc, char **argv)
+int	main(int argc, char *argv[])
 {
-	int	i;
+	char	*value;
+	char	*filename;
+	t_value	*vals;
 
-	i = 1;
-	if (argc < 2 || argc > 3)
-	{
-		write (1, "Erreur argument\n", 22);
-	}
 	if (argc == 2)
+		filename = ft_strdup("numbers.dict", 12);
+	else if (argc == 3)
+		filename = argv[1];
+	else
 	{
-		/*ft_atoi(argv[i]);*/
-		ft_verif(argv[i]);
-		ft_strlens(argv[i]);
+		ft_putstr("Expected 2 or 3 arguments\n");
+		return (1);
 	}
-	return (0);
+	value = argv[argc - 1];
+	if (!ft_verify(value))
+		return (1);
+	vals = (t_value *)malloc(
+			sizeof(t_value) * (ft_count_lines(filename) + 1));
+	ft_readdict(vals, filename);
+	write_all(vals, value);
+	ft_free_dict(vals);
 }
