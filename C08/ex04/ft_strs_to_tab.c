2615662be3f55959323c03a5f6c6d4240484b017
ckelyan <66877262+ckelyan@users.noreply.github.com>
2024-09-11 10:51:57 +0200

    C08 fix maybe

--- C08/ex04/ft_strs_to_tab.c
+++ C08/ex04/ft_strs_to_tab.c
@@ -6,7 +6,7 @@
 /*   By: kcsajka <kcsajka@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 15:46:32 by kcsajka           #+#    #+#             */
-/*   Updated: 2024/09/10 18:46:48 by kcsajka          ###   ########.fr       */
+/*   Updated: 2024/09/11 00:52:32 by kcsajka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,20 @@ int	ft_strlen(char *str)
 	int	i;
 
 	i = 0;
+	if (!str)
+		return (0);
 	while (str[i])
 		i++;
 	return (i);
+}
+
+void	free_upto(t_stock_str *arr, int size)
+{
+	int	i;
+
+	i = -1;
+	while (i < size && arr[i].copy)
+		free(arr[i].copy);
 }
 
 char	*ft_strdup(char *str)
@@ -30,6 +41,8 @@ char	*ft_strdup(char *str)
 
 	i = -1;
 	dest = (char *)malloc(ft_strlen(str) + 1);
+	if (!dest)
+		return (0);
 	while (str[++i])
 		dest[i] = str[i];
 	dest[i] = '\0';
@@ -41,16 +54,24 @@ struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
 	t_stock_str	*res;
 	int			i;
 
-	if (ac < 1)
+	if (ac < 1 || !*av)
 		return (0);
 	res = (t_stock_str *)
 		malloc(sizeof(t_stock_str) * (ac + 1));
+	if (!res)
+		return (0);
 	i = -1;
 	while (++i < ac)
 	{
 		res[i].size = ft_strlen(av[i]);
 		res[i].str = av[i];
 		res[i].copy = ft_strdup(av[i]);
+		if (!res[i].copy)
+		{
+			free_upto(res, i);
+			free(res);
+			return (0);
+		}
 	}
 	res[i].str = 0;
 	return (res);
--- C08/ex04/main.c
+++ C08/ex04/main.c
@@ -5,5 +5,6 @@ void ft_show_tab(struct s_stock_str *tab);
 int	main(int argc, char *argv[])
 {
 	struct s_stock_str *tab = ft_strs_to_tab(argc - 1, argv + 1);
-	ft_show_tab(tab);
+	if (tab)
+		ft_show_tab(tab);
 }
